b0d0a03308919376dc236b5402d27eb6f53b45e8
Noelbiggs28 <noelbiggs28@gmail.com>
2023-06-07 22:47:27 -1000

    completed counter and added thank you page

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # flask --app app.py --debug run
 # sqlite3 database.db
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import sqlite3
 
 
@@ -8,7 +8,19 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    if request.method == "GET" or request.method == "POST":
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT COUNT(*) FROM suggestions")
+        result = cursor.fetchone()
+        row_count = result[0]
+
+        cursor.close()
+        conn.close()
+
+        jsonify(row_count)
+        return render_template('index.html', row_count=row_count)
 
 @app.route("/stores")
 def stores():
@@ -18,16 +30,10 @@ def stores():
 def activities():
     return render_template('activities.html')
 
-@app.route('/suggestions', methods=["GET", "POST"])
+@app.route('/suggestions', methods=["GET"])
 def suggestions():
     if request.method == "GET":
         return render_template('suggestions.html')
-    else:
-        name = request.form.get('name')
-        description = request.form.get('description')
-        with sqlite3.connect("database.db") as con:
-            con.execute("INSERT INTO suggestions (name, description) VALUES(?, ?)", (name, description))
-        return render_template('index.html')
 
 @app.route("/keke")
 def keke():
@@ -48,3 +54,11 @@ def lighthouse():
 @app.route("/makers_market")
 def makers_market():
     return render_template('makers_market.html')
+
+@app.route("/thanks", methods=["POST"])
+def thanks():
+    name = request.form.get('name')
+    description = request.form.get('description')
+    with sqlite3.connect("database.db") as con:
+        con.execute("INSERT INTO suggestions (name, description) VALUES(?, ?)", (name, description))
+    return render_template('thanks.html')
--- static/app.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener("load", (event) =>{
-    document.querySelector("#newSuggestions").innerHTML = "there are 30 new suggestions"
-    // document.querySelector("#test").innerHTML = "there are 30 new suggestions" test
-});
